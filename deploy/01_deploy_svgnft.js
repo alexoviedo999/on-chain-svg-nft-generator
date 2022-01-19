aceba50b3643735c85222089a68d0d312416fdd7
Alejandro Oviedo <alexoviedo999@gmail.com>
2022-01-18 21:25:44 -0600

    first commit: includes all code necessary for deploying basic svg nft to rinkeby

---
+++ deploy/01_deploy_svgnft.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+let {networkConfig} = require('../helper-hardhat-config')
+
+module.exports = async ({
+    getNamedAccounts,
+    deployments,
+    getChainId
+}) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = await getChainId();
+
+    log("-------------------------");
+    log(deployer);
+    const SVGNFT = await deploy("SVGNFT", {
+        from: deployer,
+        log: true
+    })
+    log(`Deployment address: ${SVGNFT.address}`);
+    log(`You have deployed an NFT contract to ${SVGNFT.address}`);
+    let filepath = "./img/triangle.svg";
+    let svg = fs.readFileSync(filepath, { encoding: 'utf8'});
+
+    const svgNFTContract = await ethers.getContractFactory("SVGNFT");
+    const accounts = await hre.ethers.getSigners();
+    const signer = accounts[0];
+    const svgNFT = new ethers.Contract(SVGNFT.address, svgNFTContract.interface, signer);
+    const networkName = networkConfig[chainId]['name'];
+    log(`Verify with: \n npx hardhat verify --network ${networkName} ${svgNFT.address}`)
+
+    let transactionResponse = await svgNFT.create(svg);
+    let receipt = await transactionResponse.wait(1);
+    log(`You've made and NFT!`);
+    log(`You can view the tokenURI here ${await svgNFT.tokenURI(0)}`);
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,48 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require('hardhat-deploy');
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+require('dotenv').config();
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.MNEMONIC;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: PRIVATE_KEY
+      // accounts: {
+      //   mnemonic: MNEMONIC
+      // }
+    },
+    // saveDeployments: true
+  },  
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  solidity: "0.8.0",
+  namedAccounts: {
+    deployer: {
+      default: 0
+    }
+  },
+};
